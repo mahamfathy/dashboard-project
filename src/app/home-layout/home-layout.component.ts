85b126f1f9b18f53afa95fad71f5ab21acf37993
mahamfathy <mahafathyh@gmail.com>
2024-09-18 20:52:26 +0300

    appling home layout routers outlet

--- src/app/home-layout/home-layout.component.ts
+++ src/app/home-layout/home-layout.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home-layout',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, SidebarComponent, FooterComponent, RouterOutlet],
   templateUrl: './home-layout.component.html',
-  styleUrl: './home-layout.component.scss'
+  styleUrl: './home-layout.component.scss',
 })
-export class HomeLayoutComponent {
-
-}
+export class HomeLayoutComponent {}
