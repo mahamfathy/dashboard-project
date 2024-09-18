630feedf2d4cd0f1a1b0b51757de2494bb558335
mahamfathy <mahafathyh@gmail.com>
2024-09-18 20:59:44 +0300

    set app-routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { DashboardOverviewComponent } from './dashboard-overview/dashboard-overview.component';
+import { IconsComponent } from './icons/icons.component';
+import { MapsComponent } from './maps/maps.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { TableListComponent } from './table-list/table-list.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardOverviewComponent },
+  { path: 'icons', component: IconsComponent },
+  { path: 'maps', component: MapsComponent },
+  { path: 'notification', component: NotificationsComponent },
+  { path: 'user-profile', component: UserProfileComponent },
+  { path: 'table-list', component: TableListComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' },
+];
