a42e7ee21244acecacbe6af19d0148beced4d6c4
mahamfathy <mahafathyh@gmail.com>
2024-09-21 20:40:02 +0300

    sidebar html , ts and scss

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component } from '@angular/core';
+declare const bootstrap: any;
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
-
+export class SidebarComponent implements AfterViewInit {
+  constractor() {}
+  ngAfterViewInit(): void {
+    const tooltipTriggerList = Array.from(
+      document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    );
+    tooltipTriggerList.forEach((tooltipTriggerEl) => {
+      new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+  }
 }
