c3af0a0d17ca04ee3dd40fd2ec36e8b58dbc3e68
mahamfathy <mahafathyh@gmail.com>
2024-09-23 23:57:09 +0300

    create bar graph component

---
+++ src/app/dashboard-overview/graphs/bar-graph/bar-graph.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bar-graph',
+  standalone: true,
+  imports: [],
+  templateUrl: './bar-graph.component.html',
+  styleUrl: './bar-graph.component.scss'
+})
+export class BarGraphComponent {
+
+}
