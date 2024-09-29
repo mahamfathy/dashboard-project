3972cec392d428a04318d88fef6f6d98f4988742
mahamfathy <mahafathyh@gmail.com>
2024-09-29 19:24:58 +0300

    adding assets images

--- src/app/dashboard-overview/dashboard-overview.component.ts
+++ src/app/dashboard-overview/dashboard-overview.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { CardComponent } from './card/card.component';
+import { BarGraphComponent } from './graphs/bar-graph/bar-graph.component';
 import { MainGraphComponent } from './graphs/main-graph/main-graph.component';
 import { PieGraphComponent } from './graphs/pie-graph/pie-graph.component';
 
 @Component({
   selector: 'app-dashboard-overview',
   standalone: true,
-  imports: [CardComponent, MainGraphComponent, PieGraphComponent],
+  imports: [
+    CardComponent,
+    MainGraphComponent,
+    PieGraphComponent,
+    BarGraphComponent,
+  ],
   templateUrl: './dashboard-overview.component.html',
   styleUrl: './dashboard-overview.component.scss',
 })
