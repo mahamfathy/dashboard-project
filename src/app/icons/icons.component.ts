926b3250fbe3587767362687d7d0efb4e54ced11
mahamfathy <mahafathyh@gmail.com>
2024-09-29 19:55:16 +0300

    finish icon component

--- src/app/icons/icons.component.ts
+++ src/app/icons/icons.component.ts
@@ -78,10 +78,235 @@ export class IconsComponent {
     {
       no: 14,
       iconTitle: 'Video',
-      iconPath: 'assets/images/Video.png',
+      iconPath: 'assets/images/video.png',
     },
     {
       no: 15,
+      iconTitle: 'Worckplace',
+      iconPath: 'assets/images/workplace.png',
+    },
+    {
+      no: 16,
+      iconTitle: 'Building',
+      iconPath: 'assets/images/building.png',
+    },
+    {
+      no: 17,
+      iconTitle: 'Cloud',
+      iconPath: 'assets/images/cloud-network.png',
+    },
+    {
+      no: 18,
+      iconTitle: 'Idea',
+      iconPath: 'assets/images/idea.png',
+    },
+    {
+      no: 19,
+      iconTitle: 'Laptop',
+      iconPath: 'assets/images/laptop.png',
+    },
+    {
+      no: 20,
+      iconTitle: 'Like',
+      iconPath: 'assets/images/like.png',
+    },
+    {
+      no: 21,
+      iconTitle: 'Line-Chart',
+      iconPath: 'assets/images/line-chart.png',
+    },
+    {
+      no: 22,
+      iconTitle: 'Meteor-Rain',
+      iconPath: 'assets/images/meteor-rain.png',
+    },
+    {
+      no: 23,
+      iconTitle: 'Money-Bag',
+      iconPath: 'assets/images/money-bag.png',
+    },
+    {
+      no: 24,
+      iconTitle: 'MortarBoard',
+      iconPath: 'assets/images/mortarboard.png',
+    },
+    {
+      no: 25,
+      iconTitle: 'NoteBook',
+      iconPath: 'assets/images/notebook.png',
+    },
+    {
+      no: 26,
+      iconTitle: 'Presentation',
+      iconPath: 'assets/images/presentation.png',
+    },
+    {
+      no: 27,
+      iconTitle: 'Search',
+      iconPath: 'assets/images/search.png',
+    },
+    {
+      no: 28,
+      iconTitle: 'Shopping-Cart',
+      iconPath: 'assets/images/shopping-cart.png',
+    },
+    {
+      no: 29,
+      iconTitle: 'Video',
+      iconPath: 'assets/images/video.png',
+    },
+    {
+      no: 30,
+      iconTitle: 'Worckplace',
+      iconPath: 'assets/images/workplace.png',
+    },
+    {
+      no: 31,
+      iconTitle: 'Building',
+      iconPath: 'assets/images/building.png',
+    },
+    {
+      no: 32,
+      iconTitle: 'Cloud',
+      iconPath: 'assets/images/cloud-network.png',
+    },
+    {
+      no: 33,
+      iconTitle: 'Idea',
+      iconPath: 'assets/images/idea.png',
+    },
+    {
+      no: 34,
+      iconTitle: 'Laptop',
+      iconPath: 'assets/images/laptop.png',
+    },
+    {
+      no: 35,
+      iconTitle: 'Like',
+      iconPath: 'assets/images/like.png',
+    },
+    {
+      no: 36,
+      iconTitle: 'Line-Chart',
+      iconPath: 'assets/images/line-chart.png',
+    },
+    {
+      no: 37,
+      iconTitle: 'Meteor-Rain',
+      iconPath: 'assets/images/meteor-rain.png',
+    },
+    {
+      no: 38,
+      iconTitle: 'Money-Bag',
+      iconPath: 'assets/images/money-bag.png',
+    },
+    {
+      no: 39,
+      iconTitle: 'MortarBoard',
+      iconPath: 'assets/images/mortarboard.png',
+    },
+    {
+      no: 40,
+      iconTitle: 'NoteBook',
+      iconPath: 'assets/images/notebook.png',
+    },
+    {
+      no: 41,
+      iconTitle: 'Presentation',
+      iconPath: 'assets/images/presentation.png',
+    },
+    {
+      no: 42,
+      iconTitle: 'Search',
+      iconPath: 'assets/images/search.png',
+    },
+    {
+      no: 43,
+      iconTitle: 'Shopping-Cart',
+      iconPath: 'assets/images/shopping-cart.png',
+    },
+    {
+      no: 44,
+      iconTitle: 'Video',
+      iconPath: 'assets/images/video.png',
+    },
+    {
+      no: 45,
+      iconTitle: 'Worckplace',
+      iconPath: 'assets/images/workplace.png',
+    },
+    {
+      no: 46,
+      iconTitle: 'Building',
+      iconPath: 'assets/images/building.png',
+    },
+    {
+      no: 47,
+      iconTitle: 'Cloud',
+      iconPath: 'assets/images/cloud-network.png',
+    },
+    {
+      no: 48,
+      iconTitle: 'Idea',
+      iconPath: 'assets/images/idea.png',
+    },
+    {
+      no: 49,
+      iconTitle: 'Laptop',
+      iconPath: 'assets/images/laptop.png',
+    },
+    {
+      no: 50,
+      iconTitle: 'Like',
+      iconPath: 'assets/images/like.png',
+    },
+    {
+      no: 51,
+      iconTitle: 'Line-Chart',
+      iconPath: 'assets/images/line-chart.png',
+    },
+    {
+      no: 52,
+      iconTitle: 'Meteor-Rain',
+      iconPath: 'assets/images/meteor-rain.png',
+    },
+    {
+      no: 53,
+      iconTitle: 'Money-Bag',
+      iconPath: 'assets/images/money-bag.png',
+    },
+    {
+      no: 54,
+      iconTitle: 'MortarBoard',
+      iconPath: 'assets/images/mortarboard.png',
+    },
+    {
+      no: 55,
+      iconTitle: 'NoteBook',
+      iconPath: 'assets/images/notebook.png',
+    },
+    {
+      no: 56,
+      iconTitle: 'Presentation',
+      iconPath: 'assets/images/presentation.png',
+    },
+    {
+      no: 57,
+      iconTitle: 'Search',
+      iconPath: 'assets/images/search.png',
+    },
+    {
+      no: 58,
+      iconTitle: 'Shopping-Cart',
+      iconPath: 'assets/images/shopping-cart.png',
+    },
+    {
+      no: 59,
+      iconTitle: 'Video',
+      iconPath: 'assets/images/video.png',
+    },
+    {
+      no: 60,
       iconTitle: 'Worckplace',
       iconPath: 'assets/images/workplace.png',
     },
