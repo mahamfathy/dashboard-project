03628a6bcf8dd159133d4609cced3b5b78c60949
mahamfathy <mahafathyh@gmail.com>
2024-09-29 23:51:06 +0300

    user profile copmponent

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -5,8 +5,15 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.scss'
+  styleUrl: './user-profile.component.scss',
 })
 export class UserProfileComponent {
-
+  profile = {
+    name: 'Chet Faker',
+    username: '@chetfaker',
+    imagePath: 'assets/images/avatar2.jpg',
+    backgroundImage: './assets/images/background.jpg',
+    imageAlt: 'Avatar image',
+    backgroundAlt: 'Background image',
+  };
 }
