665061fa8bd3e23eeede910a0b4f4a00c3a76e83
mahamfathy <mahafathyh@gmail.com>
2024-09-30 22:49:42 +0300

    create shared models

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 const components: any = [];
-const modules = [CommonModule];
+const modules = [CommonModule, ReactiveFormsModule, FormsModule];
 @NgModule({
   declarations: [components],
   imports: [modules],
