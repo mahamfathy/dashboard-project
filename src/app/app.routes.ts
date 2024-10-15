a000cddc22df89d21f0e3ee4ac404b21e028732c
mahamfathy <mahafathyh@gmail.com>
2024-10-15 22:13:50 +0300

    editing files and folders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,19 @@
 import { Routes } from '@angular/router';
-import { DashboardOverviewComponent } from './dashboard-overview/dashboard-overview.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
 
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardOverviewComponent },
-  { path: 'icons', component: IconsComponent },
-  { path: 'maps', component: MapsComponent },
-  { path: 'notifications', component: NotificationsComponent },
-  { path: 'user-profile', component: UserProfileComponent },
-  { path: 'table-list', component: TableListComponent },
+  {
+    path: 'public',
+    loadChildren: () =>
+      import('./public/public.module').then((m) => m.PublicModule),
+  },
+  // { path: 'dashboard', component: DashboardOverviewComponent },
+  // { path: 'icons', component: IconsComponent },
+  // { path: 'maps', component: MapsComponent },
+  // { path: 'notifications', component: NotificationsComponent },
+  // { path: 'user-profile', component: UserProfileComponent },
+  // { path: 'table-list', component: TableListComponent },
+
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {
     path: '**',
