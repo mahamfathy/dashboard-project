4be86f7c87d3630acb86b75c8cf47b87c278fffc
mahamfathy <mahafathyh@gmail.com>
2024-10-18 02:15:14 +0300

    finish translating table-list

--- src/app/secure/table-list/table-list.component.ts
+++ src/app/secure/table-list/table-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { TableListService } from '../../shared/services/table-list.service';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-table-list',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './table-list.component.html',
   styleUrls: ['./table-list.component.scss'],
 })
