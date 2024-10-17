11d2aebebfcaff646f550057a05bd725bd2caed7
mahamfathy <mahafathyh@gmail.com>
2024-10-18 00:59:31 +0300

    add code in app-translate module

--- src/app/shared/modules/app-translate.module.ts
+++ src/app/shared/modules/app-translate.module.ts
@@ -1,4 +1,41 @@
-import { NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ModuleWithProviders, NgModule, Provider } from '@angular/core';
+import {
+  TranslateCompiler,
+  TranslateLoader,
+  TranslateModule,
+} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateMessageFormatCompiler } from 'ngx-translate-messageformat-compiler';
 
+const httpLoaderFactory = (http: HttpClient) =>
+  new TranslateHttpLoader(http, './assets/i18n/', '.json');
+
+const translateCompilerFactory = () => new TranslateMessageFormatCompiler();
+
+const translateLoader: Provider = {
+  provide: TranslateLoader,
+  useFactory: httpLoaderFactory,
+  deps: [HttpClient],
+};
+
+const translateCompiler: Provider = {
+  provide: TranslateCompiler,
+  useFactory: translateCompilerFactory,
+};
 @NgModule()
-export class AppTranslateModule {}
+export class AppTranslateModule {
+  static forRoot(): ModuleWithProviders<AppTranslateModule> {
+    return TranslateModule.forRoot({
+      loader: translateLoader,
+      compiler: translateCompiler,
+    });
+  }
+  static forChild(): ModuleWithProviders<AppTranslateModule> {
+    return TranslateModule.forRoot({
+      loader: translateLoader,
+      compiler: translateCompiler,
+      isolate: false,
+    });
+  }
+}
