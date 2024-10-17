e4994b0b631acd1c1c9808da57346e7de564a4fe
mahamfathy <mahafathyh@gmail.com>
2024-10-17 22:20:02 +0300

    apply interceptor in app.congif

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
+import { HandleErrorInterceptor } from './shared/interceptors/handle-error-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     importProvidersFrom(HttpClientModule),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HandleErrorInterceptor,
+      multi: true,
+    },
   ],
 };
