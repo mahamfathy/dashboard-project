690fca28177b15bf3af4909622bc4be949a548ee
mahamfathy <mahafathyh@gmail.com>
2024-10-17 20:29:56 +0300

    editing firebase json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import {} from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeLayoutComponent } from './home-layout/home-layout.component';
+import { FirebaseService } from './shared/services/firebase.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeLayoutComponent, HttpClientModule],
+  imports: [RouterOutlet, HomeLayoutComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  providers: [FirebaseService],
 })
 export class AppComponent {
   title = 'dashboard-project';
