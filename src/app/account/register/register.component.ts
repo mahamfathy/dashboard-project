8481a5d4485130e9339ed4fa8c161dcebb541aae
mahamfathy <mahafathyh@gmail.com>
2024-10-19 23:16:48 +0300

    create validation service for register to match password

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AngularMaterialModule } from '../../shared/modules/angular-material.module';
+import { ValidationService } from '../../shared/services/validation.service';
 import { SharedModule } from '../../shared/shared.module';
 
 @Component({
@@ -12,11 +13,14 @@ import { SharedModule } from '../../shared/shared.module';
 })
 export class RegisterComponent {
   constructor(private formBuilder: FormBuilder) {}
-  form = this.formBuilder.group({
-    userName: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    phoneNumber: '+1 2345 7891',
-    password: ['', [Validators.required, Validators.minLength(8)]],
-    confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
-  });
+  form = this.formBuilder.group(
+    {
+      userName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: '+1 2345 7891',
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
+    },
+    { Validators: ValidationService.mustMatch('password', 'confirmPassword') }
+  );
 }
---
+++ src/app/shared/services/validation.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ValidationService {
+  constructor() {}
+  static mustMatch(
+    controlName: string,
+    matchingControlName: string
+  ): ValidatorFn {
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      const control = formGroup.get(controlName);
+      const matchingControl = formGroup.get(matchingControlName);
+      if (!control || !matchingControl) return null;
+      if (matchingControl.errors && !matchingControl.errors['mustMatch'])
+        return null;
+      if (control.value !== matchingControl.value)
+        matchingControl.setErrors({ mustMatch: true });
+      else {
+        matchingControl.setErrors(null);
+      }
+      return null;
+    };
+  }
+}
