f0d0d1c2dfdb56afaf185ec46a375bb5a2c4719f
mahamfathy <mahafathyh@gmail.com>
2024-10-20 21:27:19 +0300

    create restapi

--- src/app/shared/firebase/firebase-url.ts
+++ src/app/shared/firebase/firebase-url.ts
@@ -1,2 +1,6 @@
 export const firebaseUrl =
   'https://visionboard-c0bc7-default-rtdb.firebaseio.com/';
+export const firebaseRestApi =
+  'https://identitytoolkit.googleapis.com/v1/accounts:';
+export const apiKey = 'AIzaSyDnTblq6EXRbk0k97MwoyNvI7zlpNFyfMY';
+export const signUpUrl = `${firebaseRestApi}signUp?key=${apiKey}`;
