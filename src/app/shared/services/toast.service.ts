ada79ff69a21cfe9007b33092c4d66c6813a056d
mahamfathy <mahafathyh@gmail.com>
2024-10-22 02:25:11 +0300

    added toastr

--- src/app/shared/services/toast.service.ts
+++ src/app/shared/services/toast.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ToastService {
-
-  constructor() { }
+  constructor(private toastr: ToastrService) {}
+  showSuccess(message?: string, title?: string): void {
+    this.toastr.success(message, title);
+  }
+  showError(message?: string, title?: string): void {
+    this.toastr.error(message, title);
+  }
 }
