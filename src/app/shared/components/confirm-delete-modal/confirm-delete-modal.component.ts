b112b3b222554f51e33746e90eb0798bdcaac23c
mahamfathy <mahafathyh@gmail.com>
2024-10-24 21:16:50 +0300

    create dialog service

--- src/app/shared/components/confirm-delete-modal/confirm-delete-modal.component.ts
+++ src/app/shared/components/confirm-delete-modal/confirm-delete-modal.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Employee } from '../../models/employee';
+import { SharedModule } from '../../shared.module';
 
 @Component({
   selector: 'app-confirm-delete-modal',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './confirm-delete-modal.component.html',
-  styleUrl: './confirm-delete-modal.component.scss'
+  styleUrl: './confirm-delete-modal.component.scss',
 })
 export class ConfirmDeleteModalComponent {
+  readonly dialogRef = inject(MatDialogRef<ConfirmDeleteModalComponent>);
+  readonly employee = inject<Employee>(MAT_DIALOG_DATA);
+  readonly animal = model(this.data.animal);
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/shared/modules/angular-material.module.ts
+++ src/app/shared/modules/angular-material.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -14,6 +20,10 @@ const modules = [
   MatIconModule,
   MatMenuModule,
   MatTableModule,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogTitle,
 ];
 @NgModule({
   declarations: [],
