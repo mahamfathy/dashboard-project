a1430c3e740f66315b8948dbf800749837489528
mahamfathy <mahafathyh@gmail.com>
2024-10-24 20:58:05 +0300

    adding confirm delete employee dialog

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { AccountComponent } from './account/account.component';
 import { LoginComponent } from './account/login/login.component';
 import { RegisterComponent } from './account/register/register.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 import { secureGuard } from './shared/guards/secure.guard';
 
 export const routes: Routes = [
---
+++ src/app/shared/components/confirm-delete-modal/confirm-delete-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-delete-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirm-delete-modal.component.html',
+  styleUrl: './confirm-delete-modal.component.scss'
+})
+export class ConfirmDeleteModalComponent {
+
+}
