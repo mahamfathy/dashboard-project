28a6c57aba545423fdb243821bf8e19447be2677
mahamfathy <mahafathyh@gmail.com>
2024-10-25 20:11:22 +0300

    apply logout method in sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,8 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { LanguageService } from '../shared/services/language.service';
 import { SharedModule } from '../shared/shared.module';
+import { LocalStorageService } from '../shared/services/local-storage.service';
+import { NavigationService } from '../shared/services/navigation.service';
 declare const bootstrap: any;
 @Component({
   selector: 'app-sidebar',
@@ -10,7 +12,8 @@ declare const bootstrap: any;
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent implements AfterViewInit {
-  constructor(private languageService: LanguageService) {}
+  constructor(private languageService: LanguageService, private localStorageService: LocalStorageService,
+    private navigationService: NavigationService) {}
   profileName = 'ABC';
   readonly arabic = 'ar';
   readonly english = 'en';
@@ -25,5 +28,9 @@ export class SidebarComponent implements AfterViewInit {
   }
   changeLanguage(lang: string): void {
     this.languageService.changeLanguage(lang);
+  }
+    logout(): void {
+    this.localStorageService.removeItem('token');
+    this.navigationService.navigateByUrl('account/login');
   }
 }
