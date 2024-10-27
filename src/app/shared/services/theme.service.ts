f9985250730b20e60c81d65808afaa2a10c3e04f
mahamfathy <mahafathyh@gmail.com>
2024-10-27 23:49:28 +0300

    create theme service

---
+++ src/app/shared/services/theme.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { LocalStorageService } from './local-storage.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private readonly themeKey = 'theme';
+  private readonly defaultTheme = 'light';
+  private readonly darkTheme = 'dark';
+  constructor(private localStorageService: LocalStorageService) {}
+
+  setTheme(theme: string): void {
+    this.localStorageService.setItem(this.themeKey, theme);
+    this.applyTheme(theme);
+  }
+  getTheme(): string {
+    return this.localStorageService.getItem(this.themeKey) || this.defaultTheme;
+  }
+
+  isDarkTheme(): boolean {
+    return this.getTheme() === this.darkTheme;
+  }
+  toggleTheme(): void {
+    this.setTheme(this.isDarkTheme() ? this.defaultTheme : this.darkTheme);
+  }
+  applyTheme(theme: string): void {
+    const body = document.body;
+    body.classList.remove(this.defaultTheme, this.darkTheme);
+    body.classList.add(
+      theme === this.darkTheme ? this.darkTheme : this.defaultTheme
+    );
+  }
+}
